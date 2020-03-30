bc735922bd9048e2fc3cfcb28bbdfeaa645a1907
yohom <yohombao@qq.com>
2020-03-30 13:17:02 +0800

    enhance: 加入demo

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:decorated_flutter/decorated_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:xftts_fluttify/xftts_fluttify.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+  XfTTS.init('5de4a580');
+}
 
 class MyApp extends StatefulWidget {
   @override
@@ -15,8 +20,17 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Text('Running on:\n'),
+        body: DecoratedColumn(
+          children: <Widget>[
+            Text('需要转换的文字'),
+            SPACE_BIG_VERTICAL,
+            RaisedButton(
+              onPressed: () {
+                XfTTS.startSpeaking('哈哈哈');
+              },
+              child: Text('开始转换'),
+            ),
+          ],
         ),
       ),
     );
