cc65428853fab94c36d2851ab17d9740adb73a72
yohom <yohombao@qq.com>
2020-03-30 12:34:33 +0800

    feat: 初始版本 包含基本接口

--- test/xftts_fluttify_test.dart
+++ test/xftts_fluttify_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:xftts_fluttify/xftts_fluttify.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('xftts_fluttify');
@@ -15,9 +14,5 @@ void main() {
 
   tearDown(() {
     channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(await XfttsFluttify.platformVersion, '42');
   });
 }
