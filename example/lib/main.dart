68266efe1cffa1add27200238acb91e56c796f68
yohom <yohombao@qq.com>
2020-03-31 21:03:24 +0800

    enhance: 完善接口 完善demo

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:xftts_fluttify/xftts_fluttify.dart';
 
 void main() {
   runApp(MyApp());
-  XfTTS.init('5de4a580');
+  XfTTS.init('5e82d068');
 }
 
 class MyApp extends StatefulWidget {
@@ -13,22 +13,52 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final _controller = TextEditingController(text: '哈哈哈');
+  Synthesizer _synthesizer;
+
+  @override
+  void initState() {
+    super.initState();
+    XfTTS.createSynthesizer().then((it) => _synthesizer = it);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        resizeToAvoidBottomInset: false,
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
         body: DecoratedColumn(
+          mainAxisAlignment: MainAxisAlignment.center,
+          padding: EdgeInsets.all(kSpaceBig),
           children: <Widget>[
-            Text('需要转换的文字'),
+            TextField(controller: _controller),
             SPACE_BIG_VERTICAL,
             RaisedButton(
               onPressed: () {
-                XfTTS.startSpeaking('哈哈哈');
+                _synthesizer?.startSpeaking(_controller.text);
               },
-              child: Text('开始转换'),
+              child: Text('开始说话'),
+            ),
+            RaisedButton(
+              onPressed: () {
+                _synthesizer?.pauseSpeaking();
+              },
+              child: Text('暂停说话'),
+            ),
+            RaisedButton(
+              onPressed: () {
+                _synthesizer?.resumeSpeaking();
+              },
+              child: Text('继续说话'),
+            ),
+            RaisedButton(
+              onPressed: () {
+                _synthesizer?.stopSpeaking();
+              },
+              child: Text('停止说话'),
             ),
           ],
         ),
